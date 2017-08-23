977fdad231978575ea8d7bda19fadf6a207a8ba0
Leon Klingele <leon@struktur.de>
2017-08-23 16:39:49 +0200

    Don't support changing the 'Spreed meeting role' for unregistered users

--- templates/main.php
+++ templates/main.php
@@ -168,7 +168,8 @@ foreach ($scripts as $script) {
 				<select class="event-select pull-left"
 					ng-model="attendee.parameters.spreedmeetingrole"
 					ng-selected="attendee.parameters.spreedmeetingrole"
-					ng-options="r.val as r.displayname for r in properties.attendeeRoles" />
+					ng-options="r.val as r.displayname for r in properties.attendeeRoles"
+					ng-disabled="attendee.parameters.isregistered !== 'TRUE'" />
 				</select>
 			</label>
 		</div>
