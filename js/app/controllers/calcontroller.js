platypro/calendar

commit aead4f591d0dfd0c1703ad3c6d1df1d24eb4166e

Add a button and link to join the Spreed meeting of an event

js/app/controllers/calcontroller.js
@@ -26,8 +26,8 @@
 * Description: The fullcalendar controller.
 */
 
-app.controller('CalController', ['$scope', 'Calendar', 'CalendarService', 'VEventService', 'SettingsService', 'TimezoneService', 'VEvent', 'is', 'fc', 'EventsEditorDialogService', 'PopoverPositioningUtility', '$window', 'isPublic', 'constants',
-	function ($scope, Calendar, CalendarService, VEventService, SettingsService, TimezoneService, VEvent, is, fc, EventsEditorDialogService, PopoverPositioningUtility, $window, isPublic, constants) {
+app.controller('CalController', ['$scope', '$location', 'Calendar', 'CalendarService', 'VEventService', 'SettingsService', 'TimezoneService', 'VEvent', 'is', 'fc', 'EventsEditorDialogService', 'PopoverPositioningUtility', '$window', 'isPublic', 'constants', 'SpreedMeetingService',
+	function ($scope, $location, Calendar, CalendarService, VEventService, SettingsService, TimezoneService, VEvent, is, fc, EventsEditorDialogService, PopoverPositioningUtility, $window, isPublic, constants, SpreedMeetingService) {
 		'use strict';
 
 		is.loading = true;
@@ -298,16 +298,23 @@ app.controller('CalController', ['$scope', 'Calendar', 'CalendarService', 'VEven
 
 					// TODO(leon): Trigger event instead
 					var spreedmeeting = event.getSimpleEvent().spreedmeeting;
-					if (spreedmeeting && spreedmeeting.parameters.token) {
-						var icon = angular.element('<img>')
+					if (spreedmeeting) {
+						var token = spreedmeeting.parameters.token;
+						var $icon = angular.element('<img>')
 							.attr('src', OC.filePath('spreed', 'img', 'app.svg'))
 						;
-						var wrapper = angular.element('<div>')
+						var $container = angular.element('<div>')
 							.addClass('spreedmeeting')
-							.append(icon)
+							.append($icon)
+						;
+						var redirectUrl = SpreedMeetingService.getRoomURL(token);
+						var $wrapper = angular.element('<a>')
+							.attr('href', redirectUrl)
+							.attr('target', '_blank')
+							.append($container);
 						;
 						element.find('.fc-content')
-							.append(wrapper)
+							.append($wrapper)
 						;
 					}
 				}

css/app/eventdialog.css
@@ -331,3 +331,7 @@ button.delete:focus {
 .dropdown-menu li a:hover {
 	background: #eee;
 }
+
+.spreedmeeting-details spreed-meeting-join-button {
+	margin-top: 12px;
+}

js/app/directives/spreedmeetingDirective.js
@@ -28,6 +28,13 @@ app.directive('spreedMeetingRoomUrl', function() {
 		templateUrl: 'spreedMeetingRoomUrl.html',
 	};
 })
+.directive('spreedMeetingJoinButton', function() {
+	'use strict';
+	return {
+		restrict: 'E',
+		templateUrl: 'spreedMeetingJoinButton.html',
+	};
+})
 .directive('spreedMeetingAttendeeRoles', function() {
 	'use strict';
 	return {

templates/main.php
@@ -152,12 +152,21 @@ foreach ($scripts as $script) {
 	</script>
 
 	<script type="text/ng-template" id="spreedMeetingRoomUrl.html">
-		<div ng-show="false && properties.doScheduleMeeting">
+		<div ng-show="properties.doScheduleMeeting && getCurrentRoomURL()">
 			<label>
 				<?php p($l->t('Meeting URL:'));?> <textarea
 					name="roomurl" type="text" class="advanced--input advanced--textarea"
 					readonly="readonly" rows="1" tabindex="210">{{ getCurrentRoomURL() }}</textarea>
 			</label>
+		</div>
+	</script>
+
+	<script type="text/ng-template" id="spreedMeetingJoinButton.html">
+		<div ng-show="properties.doScheduleMeeting && getCurrentRoomURL()">
+			<a class="button"
+				ng-href="{{ getCurrentRoomURL() }}"
+				target="_blank"
+			><?php p($l->t('Join Meeting')); ?></a>
 		</div>
 	</script>
 

templates/part.spreedmeeting.php
@@ -35,7 +35,7 @@
 	<?php p($l->t('Schedule Spreed Meeting for this event'));?>
 </label>
 
-<div ng-show="properties.doScheduleMeeting">
+<div ng-show="properties.doScheduleMeeting" class="spreedmeeting-details">
 	<label>
 		<?php p($l->t('Meeting type:'));?>
 		<select class="event-select pull-left"
@@ -44,6 +44,8 @@
 			ng-options="t.val as t.displayname for t in properties.meetingTypes" />
 		</select>
 	</label>
+
+	<spreed-meeting-join-button class="display-inline-block full-width"></spreed-meeting-join-button>
 </div>
 
 <?php }?>

commit 46eb6694e1676a51d33c25479a177515f012cb26

calcontroller: Fix regression introduced in aead4f591d0dfd0c1703ad3c6d1df1d24eb4166e

js/app/controllers/calcontroller.js
@@ -298,7 +298,7 @@ app.controller('CalController', ['$scope', '$location', 'Calendar', 'CalendarSer
 
 					// TODO(leon): Trigger event instead
 					var spreedmeeting = event.getSimpleEvent().spreedmeeting;
-					if (spreedmeeting) {
+					if (spreedmeeting && spreedmeeting.parameters.token) {
 						var token = spreedmeeting.parameters.token;
 						var $icon = angular.element('<img>')
 							.attr('src', OC.filePath('spreed', 'img', 'app.svg'))